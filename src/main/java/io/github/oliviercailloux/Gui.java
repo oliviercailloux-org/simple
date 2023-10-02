ad30cd077268452fd38553fc2f46215b03b02f0b
Olivier Cailloux <olivier.cailloux@gmail.com>
2023-10-02 11:33:42 +0200

    JavaFX

---
+++ src/main/java/io/github/oliviercailloux/Gui.java
@@ -0,0 +1,15 @@
+package io.github.oliviercailloux;
+
+import javafx.application.Application;
+
+public class Gui<T> {
+	private T id;
+
+public static void main(String[] args) {
+	Application
+}
+
+	public T getId() {
+		return id;
+	}
+}
