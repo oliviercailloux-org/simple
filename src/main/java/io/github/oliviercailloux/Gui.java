a996888ffac4de400c3e67742f6a2d8613b8926c
Olivier Cailloux <olivier.cailloux@gmail.com>
2023-10-02 11:39:22 +0200

    Simpler bug copyOf

--- src/main/java/io/github/oliviercailloux/Gui.java
+++ src/main/java/io/github/oliviercailloux/Gui.java
@@ -1,19 +1,15 @@
 package io.github.oliviercailloux;
 
-import static com.google.common.base.Preconditions.checkArgument;
-
 import java.util.List;
 import java.util.stream.Stream;
 
 import com.google.common.collect.ImmutableList;
 
-import javafx.application.Application;
-
 public class Gui<T> {
 	private T id;
 
   public void copy(List<? extends List<String>> rows) {
-    /* TODO correct code that doesn’t compile. */
+    /* TODO correct code that doesn’t compile. */ 
     // this.rows = rows.stream().<Iterable<KeyboardKey>>map(r -> r).<ImmutableList<KeyboardKey>>map(r -> ImmutableList.<KeyboardKey>copyof(r)).collect(ImmutableList.toImmutableList());
     Stream<Iterable<String>> str = rows.stream().<Iterable<String>>map(r -> r);
     List<String> row0 = rows.get(0);
