7e77dafbf379cf37d9ced483ef62552778aefa60
Olivier Cailloux <olivier.cailloux@gmail.com>
2023-10-02 11:44:04 +0200

    Simpler bug

--- src/main/java/io/github/oliviercailloux/Gui.java
+++ src/main/java/io/github/oliviercailloux/Gui.java
@@ -1,5 +1,6 @@
 package io.github.oliviercailloux;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -9,11 +10,10 @@ public class Gui<T> {
 	private T id;
 
   public void copy(List<? extends List<String>> rows) {
-    /* TODO correct code that doesn’t compile. */ 
-    // this.rows = rows.stream().<Iterable<KeyboardKey>>map(r -> r).<ImmutableList<KeyboardKey>>map(r -> ImmutableList.<KeyboardKey>copyof(r)).collect(ImmutableList.toImmutableList());
-    Stream<Iterable<String>> str = rows.stream().<Iterable<String>>map(r -> r);
     List<String> row0 = rows.get(0);
     ImmutableList.copyOf(row0);
-    Stream<ImmutableList<String>> str2 = str.<ImmutableList<String>>map(r -> ImmutableList.<String>copyof(r));
+
+    Stream<Collection<String>> str = rows.stream().<Collection<String>>map(r -> r);
+    Stream<ImmutableList<String>> str2 = str.map(r -> ImmutableList.<String>copyof(r));
   }
 }
